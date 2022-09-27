63cb0f418f8192dcddc57dfcd0a28ca32ff0166f
geekbrains <info@geekbrains>
2022-09-27 23:31:44 +0500

    требуется помощь, объясните плз как исправить мои ошибки

--- src/App.vue
+++ src/App.vue
@@ -5,102 +5,113 @@
       <input v-model.number="operand1" type="text" />
       {{ operator }}
       <input v-model.number="operand2" type="text" />
-      <!-- Для примера у v-model есть такие модификаторы:
-      ● .trim - автоматически обрезать пробелы в начале и в конце строки
-      ● .lazy - v-model синхронизирует ввод с данными по событию input, чтобы использовать для
-      синхронизации после события change
-      ● .number - для автоматического приведения введённого пользователем к Number -->
       =>
       <input v-model="result" type="text" readonly />
     </div>
+    <div v-if="error" class="error"> {{error}}</div>
 
-
-
-    <div v-if="error" class="error">{{error}}</div>
     <div class="keyboard">
       <div class="row">
-        <!-- <button v-on:click="getResult('+')">+</button> -->
-        <!-- v-on:click = @click -->
-        <!-- <button v-for="item in operators" v-bind:key="item" @click="getResult (item)">{{ item }}</button> -->
-        <!-- v-for нужна при переборе элементов для получения индекса элемента в массиве:-->
-        <button v-for="(item, index) in operators" :key="index" @click="setOperator(item)">{{ item }}</button>
+        <button v-for="(item, index) in operators" v-bind:key="index" @click="setOperator(item)">{{ item }}</button>
       </div>
-
-
-      <input type="checkbox" id="checkbox" @click="checked = !checked" v-model="sqreenKeyboard" />
-      <br>
-      Отобразить экранную клавиатуру
-      <br>
-      <div v-if="sqreenKeyboard"></div>
-      <button v-for="(item, index) in keys" :key="index" @click="setKeys(item)"> {{item}}</button>
     </div>
-  </div>
-  <ul>
-    <li v-for="[time, msg] in Object.entries(log)" :key="time">{{time}}: {{msg}}</li>
-  </ul>
+    <Second v-bind:username='author'> </Second>
+    <ul>
+      <li v-for="[time,msg] in Object.entries(log)" :key="time">{{time}}: {{msg}} </li>
+    </ul>
 
+
+
+    <input type="checkbox" id="checkbox" @click="checked = !checked" v-model="checked">
+    <label for=" checkbox"> Отобразить экранную клавиатуру</label>
+
+    <div class="sqreenKeyboard" v-if="checked">
+      <button v-for="(item, index) in numbers" :key="index" @click="getNumber(item)">
+        {{item}}</button>
+
+      <!-- <keyboard @addNum="addNumber" @delNum="delNumber" /> -->
+    </div>
+
+
+  </div>
 </template>
 
 <script>
+import Second from './components/Second.vue'
 
 export default {
-  name: 'App',
+  name: "App",
+  comments: {
+    Second
+  },
   data() {
     return {
+      operator: '+',
       operand1: 0,
       operand2: 0,
-      operator: '+',
       error: "",
-      operators: ['+', '-', '*', '/', '**', '//'],
-      keys: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '<---'],
-      log: {}
-    }
+      operators: ["+", "-", "*", "/", "**", "//"],
+      checked: [],
+      numbers: [0, 1, 2, 3, 5, 6, 7, 8, 9],
+      author: 'Admin',
+      log: {},
+      checked: false
+
+    };
   },
   computed: {
     result() {
-      this.error = '';
-      //this.log[new Date().toTimeString()] = `${this.operand1} ${operator} ${this.operand2}` (в данном случае не будет обновлять строку при повторном нажатии)
-      //this.$set(this.log, new Date().toTimeString(), `${this.operand1} ${operator} ${this.operand2}`)
-      switch (this.operator) {
-        case '+':
-          return this.operand1 + this.operand2
-          break;
-        case '*':
-          return this.operand1 * this.operand2
-          break;
-        case '-':
-          return this.operand1 - this.operand2
-          break;
-        case '/':
-          if (this.operand2 == 0) {
-            this.error = 'На ноль делить нельзя'
-          } else {
-            return this.operand1 / this.operand2
-          }
-          break;
-        case '**':
-          return this.operand1 ** this.operand2
-          break;
-        case '//':
-          return Math.floor(this.operand1 / this.operand2)
-          break;
+      this.error = "";
+      //this.log[new Date().toTimeString()] = `${this.operand1} ${operator} ${this.operand2}`
+      //  this.$set(this.log, new Date().toTimeString(), `${this.operand1} ${operator} ${this.operand2}`)
 
+      switch (this.operator) {
+        case "+":
+          return this.operand1 + this.operand2;
+          break;
+        case "-":
+          return this.operand1 - this.operand2;
+          break;
+        case "*":
+          return this.operand1 * this.operand2;
+          break;
+        case "/":
+          if (this.operand2 == 0) {
+            this.error = "На ноль делить нельзя";
+          }
+          else {
+            return this.operand1 / this.operand2;
+            break;
+          }
+        case "**":
+          return this.operand1 ** this.operand2;
+          break;
+        case "//":
+          return Math.floor(this.operand1 / this.operand2);
+          break;
         default:
           return 0
           break;
+
       }
 
     }
   },
+
+
   methods: {
     setOperator(operator) {
       this.operator = operator
     },
     getResult(operator) {
+
     },
-    // setKeys() {
-    //   this.keys = this.keys
-    // }
+    getNumber(numbers) {
+      this.operand1 = numbers;
+      this.operand2 = numbers;
+    },
+    components: { Second }
+
   }
 }
 </script>
@@ -116,12 +127,10 @@ export default {
 }
 
 .error {
-  margin: 30px;
-  padding: 20 px;
-  border: 1px solid red;
-  background-color: blue;
-  color: #cf1b4b
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid rgb(112, 55, 55);
+  background-color: rgb(231, 89, 89);
+  color: rgb(112, 55, 55);
 }
 </style>
-
-
